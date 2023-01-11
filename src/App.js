008a5ce3489b1f00266418305af8cc0081f0c7ae
Mixchelle <michelle.biologia@hotmail.com>
2023-01-11 20:51:14 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <Switch>
       <Route path="/" component={ Login } exact />
+      <Route path="/game" component={ Game } exact />
     </Switch>
   );
 }
