d71c8e9996114c5c59b528826c98437b2b218cae
Vinicius <vpozzatto@gmail.com>
2023-01-13 19:08:23 -0300

    main-group-1-req-16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 import Header from '../components/Header';
 
 export default class Feedbacks extends Component {
@@ -6,7 +8,12 @@ export default class Feedbacks extends Component {
     return (
       <div>
         <Header />
-        <h1>Feedback</h1>
+        <Link to="/ranking">
+          <Button
+            testId="btn-ranking"
+            btnLabel="Ranking"
+          />
+        </Link>
       </div>
     );
   }
