249ada695c63b3044293509b48e5ef856bb308b9
Gabriela Muniz <gabrielanamu@live.com>
2023-01-14 11:06:22 -0300

    feedback changes

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
-export default class Feedbacks extends Component {
+const three = 3;
+
+class Feedbacks extends Component {
   render() {
+    const { score } = this.props;
     return (
       <div>
         <Header />
-        <h1 data-testid="feedback-text">Feedback</h1>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  score: state.gamePage.score,
+});
+
+Feedbacks.propTypes = {
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedbacks);
